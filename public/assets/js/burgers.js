5e020d89c0802059b3aee4f9a07181119fd11a83
AndyMMunro <agovitadventures@gmail.com>
2020-10-12 20:07:58 -0700

    changed burger-block data-newdevoured to lower case and solved the issue.

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -21,24 +21,3 @@ console.log(devouredId)
     );
   });
 });
-// $(".create-form").on("submit", function (event) {
-//   // Make sure to preventDefault on a submit event.
-//   event.preventDefault();
-
-//   var newBurger = {
-//       name: $("#ca").val().trim(),
-//       devoured: true
-//   };
-
-//   // Send the POST request.
-//   $.ajax("/api/burger", {
-//       type: "POST",
-//       data: newBurger
-//   }).then(
-//       function () {
-//           console.log("created new burger");
-//           // Reload the page to get the updated list
-//           location.reload();
-//       }
-//   );
-// });
