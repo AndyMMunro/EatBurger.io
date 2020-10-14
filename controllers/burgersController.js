ceaa1a54967c86a79bba1b51be682d4b6056603d
AndyMMunro <agovitadventures@gmail.com>
2020-10-13 17:17:44 -0700

    was borken due to no closing bracket in the public burger.js file for the very first function

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -17,14 +17,12 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  burger.create([
-    "name", "devoured"
-  ], [
-    req.body.name, req.body.devoured
-  ], function(result) {
+  console.log(req.body)
+  burger.create(["name", "devoured"], [ req.body.name, req.body.devoured ], function(result) {
     // Send back the ID of the new quote
-    res.reload("/");
-  });
+    // res.reload("/");
+    res.json({ id: result.insertId });
+ });
 });
 
 router.put("/api/burgers/:id", function(req, res) {
