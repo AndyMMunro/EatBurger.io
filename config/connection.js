ffa3fb568c9c29eca028e17de2fd01d31399d5e3
AndyMMunro <agovitadventures@gmail.com>
2020-10-28 13:05:20 -0700

    package.json changes for heroku set up and deployment

--- config/connection.js
+++ config/connection.js
@@ -4,23 +4,14 @@ var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Password",
-  database: "burgers_db"
-});
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Webdev_2020!!',
+    database: 'burgers_db'
+  });
+};
 
-// Make connection.
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-// Export connection for our ORM to use.
+connection.connect();
 module.exports = connection;
