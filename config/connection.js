3c76a4b9fc7d695c24cf333088e70307df3d531a
AndyMMunro <agovitadventures@gmail.com>
2020-10-28 12:48:04 -0700

    package.json changes for heroku set up and deployment

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,15 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
 var connection = mysql.createConnection({
-  host: "zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  host: "zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
   port: 3306,
   user: "n4lcr32w49wbmtu3",
-  password: "fwybhggfrjfipire",
+  password: "fgsz6odu50iia3n0",
   database: "v03fkkierwar5rl0"
 });
 
